9d898c8f79bfdb21f164374353bbe10f0f1936eb
Marcos A. Verdura <mav.unlam@gmail.com>
2023-09-18 23:51:39 -0300

    eliminado DataHeroConverter.kt y reemplazado por extensiones en DataHero.kt y en HeroEntity.kt. Optimización inicial en HeroRepository.kt.

--- app/src/main/java/ar/edu/unlam/mobile/scaffold/domain/hero/DataHero.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/domain/hero/DataHero.kt
@@ -1,5 +1,8 @@
 package ar.edu.unlam.mobile.scaffold.domain.hero
 
+import ar.edu.unlam.mobile.scaffold.data.database.entities.AppearanceEntity
+import ar.edu.unlam.mobile.scaffold.data.database.entities.BiographyEntity
+import ar.edu.unlam.mobile.scaffold.data.database.entities.HeroEntity
 import com.google.gson.annotations.SerializedName
 
 data class DataHero(
@@ -14,3 +17,40 @@ data class DataHero(
     @SerializedName("work") val work: Work = Work(),
     val isFavorite: Boolean = false
 )
+
+fun DataHero.toHeroEntity(): HeroEntity {
+    return HeroEntity(
+        appearance = appearanceToAppearanceEntity(this.appearance),
+        biography = biographyToBiographyEntity(this.biography),
+        id = this.id.toInt(),
+        connections = this.connections,
+        image = this.image,
+        name = this.name,
+        powerstats = this.powerstats,
+        work = this.work,
+        isFavorite = this.isFavorite
+    )
+}
+
+private fun appearanceToAppearanceEntity(app: Appearance): AppearanceEntity {
+    return AppearanceEntity(
+        height = app.height.toString(),
+        weight = app.weight.toString(),
+        eyeColor = app.eyeColor,
+        gender = app.gender,
+        hairColor = app.hairColor,
+        race = app.race
+    )
+}
+
+fun biographyToBiographyEntity(bio: Biography): BiographyEntity {
+    return BiographyEntity(
+        aliases = bio.aliases.toString(),
+        alignment = bio.alignment,
+        alterEgos = bio.alterEgos,
+        firstAppearance = bio.firstAppearance,
+        fullName = bio.fullName,
+        placeOfBirth = bio.placeOfBirth,
+        publisher = bio.publisher
+    )
+}
