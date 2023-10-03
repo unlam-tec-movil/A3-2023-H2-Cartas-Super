f96fc3220ea395cdcd48ecf0aa7ab83d2363f6b8
Marcos A. Verdura <mav.unlam@gmail.com>
2023-10-03 08:04:31 -0300

    Creados QuizGameRepository e IQuizGameRepository.

---
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/data/repository/QuizGameRepository.kt
@@ -0,0 +1,18 @@
+package ar.edu.unlam.mobile.scaffold.data.repository
+
+import ar.edu.unlam.mobile.scaffold.domain.quiz.QuizGame
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class QuizGameRepository @Inject constructor(private val heroRepo: IHeroRepository) :
+    IQuizGameRepository {
+
+    private val QUIZ_OPTION_SIZE = 4
+    override suspend fun getNewQuizGame(): QuizGame {
+        val heroList = withContext(Dispatchers.IO) {
+            heroRepo.getRandomPlayerDeck(QUIZ_OPTION_SIZE)
+        }
+        return QuizGame(heroList)
+    }
+}
