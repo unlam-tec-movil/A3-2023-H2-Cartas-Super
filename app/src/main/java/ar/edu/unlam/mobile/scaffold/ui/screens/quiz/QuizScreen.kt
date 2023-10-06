0fd541f0891a0e6c2d0b63837cb8c4d99054ca74
Marcos A. Verdura <mav.unlam@gmail.com>
2023-10-06 09:15:46 -0300

    replaced Button for ElevatedButton in AnswerButton.

--- app/src/main/java/ar/edu/unlam/mobile/scaffold/ui/screens/quiz/QuizScreen.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/ui/screens/quiz/QuizScreen.kt
@@ -14,9 +14,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -249,12 +249,13 @@ fun AnswerButton(
     text: String = "text",
     onButtonClick: () -> Unit = {}
 ) {
-    Button(
+    ElevatedButton(
         onClick = onButtonClick,
         modifier = modifier
             .width(180.dp)
             .height(80.dp),
-        colors = ButtonDefaults.buttonColors(Color.Red)
+        colors = ButtonDefaults.buttonColors(Color.Red),
+        elevation = ButtonDefaults.buttonElevation(defaultElevation = 2.dp)
     ) {
         Text(
             text = text,
