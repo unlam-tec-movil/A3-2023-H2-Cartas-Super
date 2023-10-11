6d5e5b09738c65e8f7e6de235260409dc204105a
Marcos A. Verdura <mav.unlam@gmail.com>
2023-10-11 04:40:46 -0300

    Agregados ParallaxBackground a CollectionScreen.

--- app/src/main/java/ar/edu/unlam/mobile/scaffold/ui/screens/collection/CollectionScreen.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/ui/screens/collection/CollectionScreen.kt
@@ -1,18 +1,19 @@
 package ar.edu.unlam.mobile.scaffold.ui.screens.collection
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import ar.edu.unlam.mobile.scaffold.R
+import ar.edu.unlam.mobile.scaffold.domain.sensor.SensorData
+import ar.edu.unlam.mobile.scaffold.ui.components.ParallaxBackgroundImage
 import ar.edu.unlam.mobile.scaffold.ui.components.hero.HeroGallery
 
 @Composable
@@ -21,19 +22,29 @@ fun CollectionScreen(
     controller: NavHostController,
     viewModel: CollectionViewModelImp = hiltViewModel()
 ) {
-    Box(modifier = modifier){
-        Image(
-            painter = painterResource(id = R.drawable.fondo_coleccion),
+    val sensorData by viewModel.sensorData
+        .collectAsStateWithLifecycle(initialValue = SensorData(0f, 0f))
+
+    DisposableEffect(Unit) {
+        onDispose {
+            viewModel.cancelSensorDataFlow()
+        }
+    }
+
+    Box(modifier = modifier) {
+        ParallaxBackgroundImage(
+            modifier = Modifier.fillMaxSize(),
             contentDescription = "Pantalla Coleccion",
-            contentScale = ContentScale.FillHeight,
-            modifier = Modifier.fillMaxSize()
+            painterResourceId = R.drawable.fondo_coleccion,
+            data = sensorData
         )
+
         val isLoading = viewModel.isLoading.collectAsStateWithLifecycle()
 
-        if(isLoading.value) {
-                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        if (isLoading.value) {
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
         } else {
-            val onItemClick: (Int) -> Unit = {heroID -> controller.navigate(route = "herodetail/$heroID")}
+            val onItemClick: (Int) -> Unit = { heroID -> controller.navigate(route = "herodetail/$heroID") }
             HeroGallery(
                 modifier = Modifier.fillMaxSize(),
                 heroList = viewModel.heroList.collectAsStateWithLifecycle().value,
