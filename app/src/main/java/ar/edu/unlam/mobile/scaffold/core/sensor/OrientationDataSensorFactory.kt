981162013dcf19c1d2bd1db94deb222e93ed2811
Marcos A. Verdura <mav.unlam@gmail.com>
2023-10-16 02:49:19 -0300

    agregado Log.d en OrientationDataSensorFactory.

--- app/src/main/java/ar/edu/unlam/mobile/scaffold/core/sensor/OrientationDataSensorFactory.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/core/sensor/OrientationDataSensorFactory.kt
@@ -2,6 +2,7 @@ package ar.edu.unlam.mobile.scaffold.core.sensor
 
 import android.hardware.Sensor
 import android.hardware.SensorManager
+import android.util.Log
 import ar.edu.unlam.mobile.scaffold.core.sensor.sensordatamanager.DefaultDataManager
 import ar.edu.unlam.mobile.scaffold.core.sensor.sensordatamanager.GameRotationVectorSensorManager
 import ar.edu.unlam.mobile.scaffold.core.sensor.sensordatamanager.IOrientationDataManager
@@ -34,10 +35,22 @@ class OrientationDataSensorFactory @Inject constructor(private val sensorManager
 
     fun getOrientationDataManager(): IOrientationDataManager {
         return when (availableSensor) {
-            AvailableSensor.GAME_VECTOR -> GameRotationVectorSensorManager(sensorManager)
-            AvailableSensor.ROTATION_VECTOR -> RotationVectorSensorManager(sensorManager)
-            AvailableSensor.GRAVITY_AND_MAGNETIC_FIELD -> OrientationDataManager(sensorManager)
-            AvailableSensor.NONE -> DefaultDataManager()
+            AvailableSensor.GAME_VECTOR -> {
+                Log.d("OrientationDataSensorFactory", "Se ha instanciado GameRotationVectorSensorManager")
+                GameRotationVectorSensorManager(sensorManager)
+            }
+            AvailableSensor.ROTATION_VECTOR -> {
+                Log.d("OrientationDataSensorFactory", "Se ha instanciado RotationVectorSensorManager")
+                RotationVectorSensorManager(sensorManager)
+            }
+            AvailableSensor.GRAVITY_AND_MAGNETIC_FIELD -> {
+                Log.d("OrientationDataSensorFactory", "Se ha instanciado OrientationDataManager")
+                OrientationDataManager(sensorManager)
+            }
+            AvailableSensor.NONE -> {
+                Log.d("OrientationDataSensorFactory", "Se ha instanciado DefaultDataManager")
+                DefaultDataManager()
+            }
         }
     }
 }
