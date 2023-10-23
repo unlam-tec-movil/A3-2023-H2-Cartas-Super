31e0e64743c0a00aad5dd080332688680ac2491c
Marcos A. Verdura <mav.unlam@gmail.com>
2023-10-23 09:45:35 -0300

    creado CustomButton.

---
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/ui/components/CustomButton.kt
@@ -0,0 +1,36 @@
+package ar.edu.unlam.mobile.scaffold.ui.components
+
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ar.edu.unlam.mobile.scaffold.ui.theme.shaka_pow
+
+@Preview
+@Composable
+fun CustomButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {},
+    label: () -> String = { "Button" }
+) {
+    ElevatedButton(
+        modifier = modifier.testTag(tag = "elevated button"),
+        onClick = onClick,
+        colors = ButtonDefaults.elevatedButtonColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        elevation = ButtonDefaults.buttonElevation(defaultElevation = 3.dp)
+    ) {
+        Text(
+            modifier = Modifier.testTag(tag = "label"),
+            text = label(),
+            fontFamily = shaka_pow,
+            color = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    }
+}
