76c05fbb5819713103dae0b3f0f9442dc406d238
Marcos A. Verdura <mav.unlam@gmail.com>
2023-10-27 15:07:30 -0300

    primera versión de los test de QuizScreen.

--- app/src/androidTest/java/ar/edu/unlam/mobile/scaffold/ui/screens/quiz/QuizScreenKtTest.kt
+++ app/src/androidTest/java/ar/edu/unlam/mobile/scaffold/ui/screens/quiz/QuizScreenKtTest.kt
@@ -2,12 +2,17 @@ package ar.edu.unlam.mobile.scaffold.ui.screens.quiz
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.hasClickAction
+import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.printToLog
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
 import ar.edu.unlam.mobile.scaffold.core.sensor.sensordatamanager.DefaultDataManager
 import ar.edu.unlam.mobile.scaffold.data.database.HeroDataBase
 import ar.edu.unlam.mobile.scaffold.data.database.dao.HeroDao
@@ -16,8 +21,6 @@ import ar.edu.unlam.mobile.scaffold.data.network.IHeroApiClient
 import ar.edu.unlam.mobile.scaffold.data.repository.herorepository.HeroRepository
 import ar.edu.unlam.mobile.scaffold.data.repository.quizrepository.QuizGameRepository
 import ar.edu.unlam.mobile.scaffold.ui.theme.ComicWarTheme
-import kotlinx.coroutines.test.runTest
-import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -26,6 +29,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class QuizScreenKtTest {
 
     // compose rule is required to get access to the composable component
@@ -42,7 +46,7 @@ class QuizScreenKtTest {
     fun setup() {
         heroDataBase = Room.inMemoryDatabaseBuilder(
             context = ApplicationProvider.getApplicationContext(),
-            klass = HeroDataBase::class.java
+            klass = HeroDataBase::class.java,
         ).build()
 
         heroDao = heroDataBase.getHeroDao()
@@ -63,11 +67,6 @@ class QuizScreenKtTest {
         quizRepo = QuizGameRepository(heroRepo = heroRepository)
     }
 
-    @After
-    fun tearDown() {
-        heroDataBase.close()
-    }
-
     @Test
     fun whenHavingDefaultStates_VerifyIfAllViewsExists() {
         composeTestRule.setContent { // setting our composable as content for test
@@ -82,6 +81,7 @@ class QuizScreenKtTest {
         composeTestRule.onNodeWithTag("title").assertExists()
         composeTestRule.onNodeWithTag("hero image").assertExists()
         composeTestRule.onNodeWithTag("answer panel").assertExists()
+        composeTestRule.onNodeWithTag("Result popup").assertDoesNotExist()
     }
 
     @Test
@@ -99,10 +99,11 @@ class QuizScreenKtTest {
         composeTestRule.onNodeWithTag("title").assertDoesNotExist()
         composeTestRule.onNodeWithTag("hero image").assertDoesNotExist()
         composeTestRule.onNodeWithTag("answer panel").assertDoesNotExist()
+        composeTestRule.onNodeWithTag("Result popup").assertDoesNotExist()
     }
 
     @Test
-    fun whenShowPopupIsTrueVerifyResultPopupExists() {
+    fun whenShowPopupIsTrue_VerifyResultPopupExists() {
         composeTestRule.setContent { // setting our composable as content for test
             ComicWarTheme {
                 QuizUi(
@@ -115,7 +116,20 @@ class QuizScreenKtTest {
     }
 
     @Test
-    fun whenShowIsTrueInQuizPopupResultVerifyAllViewsExist() {
+    fun whenShowPopupIsFalse_VerifyResultPopupDoesNotExist() {
+        composeTestRule.setContent { // setting our composable as content for test
+            ComicWarTheme {
+                QuizUi(
+                    modifier = Modifier.fillMaxSize(),
+                    showPopup = { false }
+                )
+            }
+        }
+        composeTestRule.onNodeWithTag("Result popup").assertDoesNotExist()
+    }
+
+    @Test
+    fun whenShowIsTrueInQuizPopupResult_VerifyAllViewsExist() {
         composeTestRule.setContent {
             ComicWarTheme {
                 QuizResultPopup(
@@ -142,8 +156,9 @@ class QuizScreenKtTest {
         composeTestRule.onNodeWithTag("option 4 button").assertExists()
     }
 
+    @OptIn(ExperimentalTestApi::class)
     @Test
-    fun whenClickingOption1_TheAlertDialogMustExist() = runTest {
+    fun whenClickingOption1_TheAlertDialogMustExist() {
         quizRepo = QuizGameRepository(heroRepository)
         composeTestRule.setContent {
             ComicWarTheme {
@@ -155,10 +170,27 @@ class QuizScreenKtTest {
                 )
             }
         }
+        composeTestRule.waitUntilNodeCount(matcher = hasClickAction(), count = 4, timeoutMillis = 10000L)
+        composeTestRule.onAllNodes(
+            matcher = hasClickAction(),
+            useUnmergedTree = true
+        )
+            .printToLog(tag = "compose_test", maxDepth = 10)
         composeTestRule.onNodeWithTag(testTag = "option 1 button", useUnmergedTree = true).performClick()
-        composeTestRule.onNodeWithTag("alert dialog").assertExists()
+        composeTestRule.onAllNodes(
+            matcher = hasClickAction(),
+            useUnmergedTree = true
+        )
+            .printToLog(tag = "copose_test", maxDepth = 10)
+
+        composeTestRule.waitUntilExactlyOneExists(
+            matcher = hasTestTag(testTag = "Result popup"),
+            timeoutMillis = 8_165L
+        )
+        composeTestRule.onNodeWithTag("Result popup", useUnmergedTree = true).assertExists()
     }
 
+    @OptIn(ExperimentalTestApi::class)
     @Test
     fun whenClickingOption2_TheAlertDialogMustExist() {
         composeTestRule.setContent {
@@ -171,10 +203,27 @@ class QuizScreenKtTest {
                 )
             }
         }
+        composeTestRule.waitUntilNodeCount(matcher = hasClickAction(), count = 4, timeoutMillis = 10000L)
+        composeTestRule.onAllNodes(
+            matcher = hasClickAction(),
+            useUnmergedTree = true
+        )
+            .printToLog(tag = "compose_test", maxDepth = 10)
         composeTestRule.onNodeWithTag(testTag = "option 2 button", useUnmergedTree = true).performClick()
-        composeTestRule.onNodeWithTag("alert dialog").assertExists()
+        composeTestRule.onAllNodes(
+            matcher = hasClickAction(),
+            useUnmergedTree = true
+        )
+            .printToLog(tag = "copose_test", maxDepth = 10)
+
+        composeTestRule.waitUntilExactlyOneExists(
+            matcher = hasTestTag(testTag = "Result popup"),
+            timeoutMillis = 8_165L
+        )
+        composeTestRule.onNodeWithTag("Result popup", useUnmergedTree = true).assertExists()
     }
 
+    @OptIn(ExperimentalTestApi::class)
     @Test
     fun whenClickingOption3_TheAlertDialogMustExist() {
         composeTestRule.setContent {
@@ -187,10 +236,27 @@ class QuizScreenKtTest {
                 )
             }
         }
+        composeTestRule.waitUntilNodeCount(matcher = hasClickAction(), count = 4, timeoutMillis = 10000L)
+        composeTestRule.onAllNodes(
+            matcher = hasClickAction(),
+            useUnmergedTree = true
+        )
+            .printToLog(tag = "compose_test", maxDepth = 10)
         composeTestRule.onNodeWithTag(testTag = "option 3 button", useUnmergedTree = true).performClick()
-        composeTestRule.onNodeWithTag("alert dialog").assertExists()
+        composeTestRule.onAllNodes(
+            matcher = hasClickAction(),
+            useUnmergedTree = true
+        )
+            .printToLog(tag = "copose_test", maxDepth = 10)
+
+        composeTestRule.waitUntilExactlyOneExists(
+            matcher = hasTestTag(testTag = "Result popup"),
+            timeoutMillis = 8_165L
+        )
+        composeTestRule.onNodeWithTag("Result popup", useUnmergedTree = true).assertExists()
     }
 
+    @OptIn(ExperimentalTestApi::class)
     @Test
     fun whenClickingOption4_TheAlertDialogMustExist() {
         composeTestRule.setContent {
@@ -203,7 +269,23 @@ class QuizScreenKtTest {
                 )
             }
         }
+        composeTestRule.waitUntilNodeCount(matcher = hasClickAction(), count = 4, timeoutMillis = 10000L)
+        composeTestRule.onAllNodes(
+            matcher = hasClickAction(),
+            useUnmergedTree = true
+        )
+            .printToLog(tag = "compose_test", maxDepth = 10)
         composeTestRule.onNodeWithTag(testTag = "option 4 button", useUnmergedTree = true).performClick()
-        composeTestRule.onNodeWithTag("alert dialog").assertExists()
+        composeTestRule.onAllNodes(
+            matcher = hasClickAction(),
+            useUnmergedTree = true
+        )
+            .printToLog(tag = "copose_test", maxDepth = 10)
+
+        composeTestRule.waitUntilExactlyOneExists(
+            matcher = hasTestTag(testTag = "Result popup"),
+            timeoutMillis = 8_165L
+        )
+        composeTestRule.onNodeWithTag("Result popup", useUnmergedTree = true).assertExists()
     }
 }
