80543859f1ee076e3ee88e44b486e771c4e2cc72
Marcos A. Verdura <mav.unlam@gmail.com>
2023-11-04 00:27:52 -0300

    refactorizado v1 de ActionMenu. Eliminado SelectStatMultiplier.kt porque era redundante.

--- app/src/main/java/ar/edu/unlam/mobile/scaffold/ui/components/ActionMenu.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/ui/components/ActionMenu.kt
@@ -1,22 +1,20 @@
 package ar.edu.unlam.mobile.scaffold.ui.components
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import ar.edu.unlam.mobile.scaffold.domain.heroDuel.Stat
 
 @Preview(showBackground = true)
@@ -28,32 +26,30 @@ fun ActionMenu(
     useMultiplierX2: (Boolean) -> Unit = {},
     onFightClick: () -> Unit = {}
 ) {
+    var checked by rememberSaveable {
+        mutableStateOf(false)
+    }
     Row(
         modifier = modifier,
         horizontalArrangement = Arrangement.SpaceAround,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        SelectCardStat(modifier = Modifier.width(160.dp), onClick = onClickSelectedStat)
-        Spacer(modifier = Modifier.size(8.dp))
-        SelectStatMultiplier(
-            onUseMultiplier = useMultiplierX2,
-            enabled = canMultix2BeUsed
-        )
-        Button(
-            colors = ButtonDefaults.buttonColors(Color.DarkGray),
-            shape = ButtonDefaults.outlinedShape,
-            onClick = {
-                onFightClick()
-            }
-        ) {
-            Text(
-                text = "¡Pelear!",
-                color = Color.White,
-                fontWeight = FontWeight.Bold,
-                style = TextStyle(
-                    fontSize = 15.sp
-                )
+        Column {
+            SelectCardStat(modifier = Modifier.width(160.dp), onClick = onClickSelectedStat)
+            CustomLabeledCheckbox(
+                enabled = { canMultix2BeUsed },
+                label = { "Multi x2:" },
+                checked = { checked },
+                onCheckedChange = {
+                    checked = it
+                    useMultiplierX2(checked)
+                }
             )
         }
+        Spacer(modifier = Modifier.size(8.dp))
+        CustomButton(
+            label = { "Pelear!" },
+            onClick = onFightClick
+        )
     }
 }
